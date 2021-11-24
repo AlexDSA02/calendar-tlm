38f71e330b15306501536a60153adb188463e38c
Areias Alexandre <alexandre.dsa02@gmail.com>
2021-11-24 02:50:54 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+
+
+const appRoutes: Routes = [
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component},
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {gsap} from "gsap";
+
+@Component({
+  selector: 'app-page1',
+  templateUrl: './page1.component.html',
+  styleUrls: ['./page1.component.css']
+})
+export class Page1Component implements OnInit {
+  arrayNbrDay : any = [1,2,3,4,5];
+  arrayNbrDay2 : any = [6,7,8,9,10];
+  arrayNbrDay3 : any = [11,12,13,14,15];
+  arrayNbrDay4 : any = [15,16,17,18,19];
+  arrayNbrDay5 : any = [20,21,22,23,24];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  test(){
+    const btnDelete = document.getElementById('btnDelete');
+    const modal = document.getElementById('modal');
+    const blockOpacity = document.getElementById('blockOpacity');
+    let t1 = gsap.timeline({defaults: { duration: 1.5}});
+    t1
+      .to(modal, {display: 'flex', duration: 0})
+      .to(modal, {display: 'flex', duration: 0})
+      .to(modal, {zIndex: 2, duration: 0})
+      .to(blockOpacity, {zIndex: 0, duration: 0})
+      .to(blockOpacity, {opacity: 0.8}, 0)
+      .to(modal, {opacity: 1},0)
+      .to(btnDelete, {display: 'block', duration: 0})
+
+
+  }
+
+  delete() {
+    const btnDelete = document.getElementById('btnDelete');
+    const modal = document.getElementById('modal');
+    const blockOpacity = document.getElementById('blockOpacity');
+    let t1 = gsap.timeline({defaults: { duration: 1.5}});
+    t1
+      .to(btnDelete, {display: 'none', duration: 0})
+      .to(modal, {display: 'none', duration: 0})
+      .to(blockOpacity, {zIndex: -1, duration: 0})
+      .to(blockOpacity, {opacity: 0, duration: 0})
+      .to(modal, {opacity: 0, duration: 0})
+  }
+}
